1bf6e419b438e45be37c64d6405e430186745057
Marc Ducret <marc.t.ducret@gmail.com>
2023-08-03 18:44:30 +0200

    Reproduce issue.

---
+++ Assets/SpawnEventStrips.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace DefaultNamespace {
+    public class SpawnEventStrips : MonoBehaviour {
+        public int    spawnCount;
+        public float3 offset;
+
+        protected void Start() {
+            VisualEffect visualEffect = GetComponent<VisualEffect>();
+
+            VFXEventAttribute eventAttribute = visualEffect.CreateVFXEventAttribute();
+
+            float3 position = float3.zero;
+
+            for (int i = 0; i < spawnCount; i++) {
+                eventAttribute.SetUint("Emit_Event_Index", (uint)i);
+                eventAttribute.SetVector3("Emit_Position", position);
+                visualEffect.SendEvent("Emit", eventAttribute);
+                position += offset;
+            }
+        }
+    }
+}
